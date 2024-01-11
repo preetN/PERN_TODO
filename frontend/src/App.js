6eaaad840e22af81edbfe22bf6a1729baee68654
Antarpreet <pnarain82@gmail.com>
2024-01-11 13:48:33 +1100

    client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 function App() {
   const [description, setDescription] = useState("");
   return <div>Hello World</div>;
---
+++ frontend/src/components/InputTodo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function InputTodo() {
+  return <div></div>;
+}
