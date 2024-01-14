ce9d0f58f80ce683c99e7e9d98a3104f388b7165
Antarpreet <pnarain82@gmail.com>
2024-01-14 15:20:12 +1100

    hfehrkjf

--- backend/dbModel.js
+++ backend/dbModel.js
@@ -1,6 +1,13 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// });
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  host: "localhost",
+  database: "pern_todo",
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
 });
 module.exports = pool;
